388d7894cee1b0449f651494a4335d745df87aee
TienDung030704 <tiendung030704@gmail.com>
2026-04-17 00:29:27 +0700

    update lại browser thành hash

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider as ReduxProvider } from "react-redux";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { store } from "./store/store";
@@ -7,11 +7,11 @@ import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <ReduxProvider store={store}>
       <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
         <App />
       </GoogleOAuthProvider>
     </ReduxProvider>
-  </BrowserRouter>,
+  </HashRouter>,
 );
